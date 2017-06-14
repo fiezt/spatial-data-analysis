c8efce463ce6f5965048ad892cee868093ba6cec
Tanner Fiez <fiezt@uw.edu>
2017-06-13 21:40:51 -0700

    clean map overlay

--- code/map_overlay.py
+++ code/map_overlay.py
@@ -2,50 +2,67 @@ import math
 
 class MapOverlay:
     def __init__(self, topleft_latlong, bottomright_latlong, pixels, resolution=1024.0):
-        #resolution is the projected resolution of the latitude and longitude coordinates
-        #to integer pixel values--a higher projected resolution results in coordinate resolution
-        #per pixel
+        """Initializing the size parameters for the image.
+
+        :param topleft_latlong: List of tuple of the lat, long pair of the 
+        top left corner of the image.
+        :param bottomright_latlong: List or tuple of the lat, long pair of the 
+        bottom right corner of the image.
+        :param pixels: List or tuple of the pixel dimensions of the image.
+        :param resolution: Float of the projected resolution of the lat, long pair.
+        """
         
-        #topleft_latlong and bottomright_latlong coorespond to the upper right and bottom left
-        #latitude and longitude coordinates visible in your Mercator projected map image
         self.res = resolution
+
         self.topleft = self.to_web_mercator(topleft_latlong)
         self.bottomright = self.to_web_mercator(bottomright_latlong)
-        
-        #the following returns the vertical and horizontal scaling factor of the projected coordinates to 
-        #the pixel size of the map image
-        #ex: pixels = [256,256]
+
+        # Calculate the scaling factors of the projected coordinates.
         self.horzscale = pixels[0]/(abs(self.bottomright[1] - self.topleft[1]))
         self.vertscale = pixels[1]/(abs(self.topleft[0] - self.bottomright[0]))
         
+        
     def to_web_mercator(self, coord, zoomlvl=1):
-        #raw latitude longitude pair to web mercator pixel position
-        #https://en.wikipedia.org/wiki/Web_Mercator
-        #1024x1024 base pixel image
-        #x = longitude
-        #y = latitude, all converted coordinate pairs are read as [latitude, longitude]
+        """Convert latitude and longitude pair to web mercator pixel position.
+
+        For more information on this see https://en.wikipedia.org/wiki/Web_Mercator.
+        
+        :param coord: List or tuple of the lat, long pair.
+        :param zoomlvl: Scaler zoom level of the image.
+
+        :return pair: Tuple of lat, long pair for pixels.
+        """
+
         lat = coord[0]
         lon = coord[1]
     
-        #latitude conversion
+        # Latitude conversion.
         lat_rad = lat * math.pi/180.0
-        yit = math.pi - math.log(math.tan( (0.25*math.pi) + (0.5*lat_rad) ))
+        yit = math.pi - math.log(math.tan((0.25*math.pi) + (0.5*lat_rad)))
         y = (self.res)/math.pi * math.pow(2,zoomlvl) * yit
         
-        #longitude conversion
+        # Longitude conversion.
         lon_rad = lon * math.pi/180.0
         x = (self.res)/math.pi * math.pow(2,zoomlvl) * (lon_rad + math.pi)
+
+        pair = (y, x)
     
-        return([y,x])
+        return pair
+
 
     def to_image_pixel_position(self, coord):
-        #raw latitude longitude pair to image pixel position
-        #lat --> vertical scale
-        #long --> horizontal scale  
+        """Convert latitude and longitude pair to image pixel position in image.
+        
+        :param coord: List or tuple of the lat, long pair.
+
+        :return position: Tuple of corresponding pixel position of lat, long pair.
+        """
+
         webmcoord = self.to_web_mercator(coord)
+
         horz = abs(webmcoord[0] - self.topleft[0])*self.horzscale
         vert = abs(webmcoord[1] - self.topleft[1])*self.vertscale
     
-        position = [int(round(vert)), int(round(horz))]
+        position = tuple([int(round(vert)), int(round(horz))])
     
-        return(position)
+        return position
